0ce1689f470d63cdd9ad2abf45e5282e1556dddd
Nate Thrasher <nthrasher@secureworks.com>
2019-07-09 15:26:56 -0600

    Added calculation of temporal score.

--- src/main/java/us/springett/cvss/Cvss.java
+++ src/main/java/us/springett/cvss/Cvss.java
@@ -27,7 +27,7 @@ import java.util.regex.Pattern;
  */
 public interface Cvss {
 
-    Pattern CVSSv2_PATTERN = Pattern.compile("AV:[NAL]\\/AC:[LMH]\\/A[Uu]:[NSM]\\/C:[NPC]\\/I:[NPC]\\/A:[NPC]");
+    Pattern CVSSv2_PATTERN = Pattern.compile("AV:[NAL]\\/AC:[LMH]\\/A[Uu]:[NSM]\\/C:[NPC]\\/I:[NPC]\\/A:[NPC]\\/E:\b(F|H|U|POC|ND)\b\\/RL:\b(W|U|TF|OF|ND)\b\\/RC:\b(C|UR|UC|ND)\b");
     Pattern CVSSv3_PATTERN = Pattern.compile("AV:[NALP]\\/AC:[LH]\\/PR:[NLH]\\/UI:[NR]\\/S:[UC]\\/C:[NLH]\\/I:[NLH]\\/A:[NLH]");
 
     /**
@@ -71,6 +71,9 @@ public interface Cvss {
             cvssV2.confidentiality(CvssV2.CIA.fromString(st.nextElement().toString().split(":")[1]));
             cvssV2.integrity(CvssV2.CIA.fromString(st.nextElement().toString().split(":")[1]));
             cvssV2.availability(CvssV2.CIA.fromString(st.nextElement().toString().split(":")[1]));
+            cvssV2.exploitability(CvssV2.Exploitability.fromString(st.nextElement().toString().split(":")[1]));
+            cvssV2.remediationLevel(CvssV2.RemediationLevel.fromString(st.nextElement().toString().split(":")[1]));
+            cvssV2.reportConfidence(CvssV2.ReportConfidence.fromString(st.nextElement().toString().split(":")[1]));
             return cvssV2;
         } else if (v3Matcher.find()) {
             // Found a valid CVSSv3 vector
--- src/main/java/us/springett/cvss/Score.java
+++ src/main/java/us/springett/cvss/Score.java
@@ -26,11 +26,17 @@ public class Score {
     private double baseScore;
     private double impactSubScore;
     private double exploitabilitySubScore;
+    private double temporalScore;
 
     public Score(double baseScore, double impactSubScore, double exploitabilitySubScore) {
+        this(baseScore, impactSubScore, exploitabilitySubScore, -1);
+    }
+
+    public Score(double baseScore, double impactSubScore, double exploitabilitySubScore, double temporalScore) {
         this.baseScore = baseScore;
         this.impactSubScore = impactSubScore;
         this.exploitabilitySubScore = exploitabilitySubScore;
+        this.temporalScore = temporalScore;
     }
 
     /**
@@ -55,5 +61,13 @@ public class Score {
      */
     public double getExploitabilitySubScore() {
         return exploitabilitySubScore;
+    }
+
+    /**
+     * Returns the temporal subscore.
+     * @return the temporal subscore
+     */
+    public double getTemporalScore() {
+        return temporalScore;
     }
 }
